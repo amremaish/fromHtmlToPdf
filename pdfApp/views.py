61619ca13618b7f2017ebe7c12a16d7d95c719e5
amremaish <amr.emaish@olx.com.eg>
2022-06-19 16:49:18 +0200

    return pdf

--- pdfApp/views.py
+++ pdfApp/views.py
@@ -1,4 +1,5 @@
 import pdfkit
+from django.http import HttpResponse
 from django.shortcuts import render
 
 html = '<!DOCTYPE html><html><body><h1>My First Heading</h1><p>My first paragraph.</p></body></html>'
@@ -6,7 +7,7 @@ html = '<!DOCTYPE html><html><body><h1>My First Heading</h1><p>My first paragrap
 
 def to_pdf(request):
     doc_pdf = pdfkit.from_string(html)
-    return html
+    return HttpResponse(doc_pdf, content_type='application/octet-stream')
 
 
 def to_pdf_func():
